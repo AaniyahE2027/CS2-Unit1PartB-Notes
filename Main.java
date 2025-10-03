0b286ee3d2db348d6268d79d9f1f3385550d15b8
Aaniyah <aaniyah.ellis2027@gbwl.org>
2025-10-03 12:42:58 +0000

    1.8 notes

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
+/**
+ * @author Aaniyah E.
+ * @since Oct 3 2025
+ * @version 1.0
+ * Notes for topics 1.8-1.15
+ */
 public class Main {
-
+   /**
+    * main method is the entry point of the program
+    * @param input is an array of String
+    * @return VOID means NO OUTPUT
+    */
    public static void main(String []args) {
       System.out.println("Hello World");
    }
